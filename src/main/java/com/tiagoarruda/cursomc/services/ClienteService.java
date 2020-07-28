8a32a8f9e19299979aeaadbce541b55e4a3b9c61
Tiago Cruz <tiagoarrudadacruz@gmail.com>
2020-07-28 09:09:10 -0300

    PUT, GET, DELETE para Cliente

--- src/main/java/com/tiagoarruda/cursomc/services/ClienteService.java
+++ src/main/java/com/tiagoarruda/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.tiagoarruda.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.tiagoarruda.cursomc.domain.Cliente;
+import com.tiagoarruda.cursomc.dto.ClienteDTO;
 import com.tiagoarruda.cursomc.repositories.ClienteRepository;
+import com.tiagoarruda.cursomc.services.exceptions.DataIntegrityException;
 import com.tiagoarruda.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,5 +28,36 @@ public class ClienteService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: "+id+", Tipo:"+Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+			repo.deleteById(id);
+		} 
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria com produtos");
+		}		
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPage,Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+		
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(), null, null);
 	}
 }
