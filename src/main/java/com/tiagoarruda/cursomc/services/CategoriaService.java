734ffb368f67c163ed334820e42def8697cb8371
Tiago Cruz <tiagoarrudadacruz@gmail.com>
2020-07-27 22:11:02 -0300

    Validacao sintatica com Bean Validantion

--- src/main/java/com/tiagoarruda/cursomc/services/CategoriaService.java
+++ src/main/java/com/tiagoarruda/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.tiagoarruda.cursomc.domain.Categoria;
+import com.tiagoarruda.cursomc.dto.CategoriaDTO;
 import com.tiagoarruda.cursomc.repositories.CategoriaRepository;
 import com.tiagoarruda.cursomc.services.exceptions.DataIntegrityException;
 import com.tiagoarruda.cursomc.services.exceptions.ObjectNotFoundException;
@@ -60,5 +61,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPage,Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
