d3577693abeb36cdd05d61c21c0c664b6f9c16f5
Tiago Cruz <tiagoarrudadacruz@gmail.com>
2020-07-29 20:39:42 -0300

    Inserindo Pedido

--- src/main/java/com/tiagoarruda/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/tiagoarruda/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.tiagoarruda.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.tiagoarruda.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 
